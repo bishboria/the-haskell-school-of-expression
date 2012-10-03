a8f1433223e6916a526bf01667a4270e8970b400
Stuart Gale <bishboria@gmail.com>
2012-10-03 16:25:08 +0100

    Adding Ex 3.01

--- chapter03/01.hs
+++ chapter03/01.hs
@@ -2,8 +2,20 @@ module Main where
 
 putString :: String -> IO ()
 putString []     = return ()
-putString (c:cs) =
-    do putChar c
-       putString cs
+putString (c:cs) = do
+    putChar c
+    putString cs
 
-main = putString "Hello World\n"
+getString :: IO String
+getString = do
+    c <- getChar
+    if (c == '\n')
+    then
+        return []
+    else do
+        cs <- getString
+        return $ c : cs
+
+main = do
+    str <- getString
+    putString str
